a8d045dd67ff221ce73682a171e2171032587720
Advik Kashyap <advikkashyap2023@gmail.com>
2025-07-21 02:03:33 +0530

    🐛 Fix: Added fetch polyfill for OpenAI on Node.js 16

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+globalThis.fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
